9e4c70cfec47320686a2880c6d747519525ee9e9
developer-ipark <mohammadali.talaei@georgebrown.ca>
2024-02-24 20:57:57 -0500

    Initial Commit

---
+++ todolist-group12/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  todolist-group12
+//
+//  Created by developer-rapidcents on 2024-02-24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ todolist-group12/todolist_group12App.swift
@@ -0,0 +1,17 @@
+//
+//  todolist_group12App.swift
+//  todolist-group12
+//
+//  Created by developer-rapidcents on 2024-02-24.
+//
+
+import SwiftUI
+
+@main
+struct todolist_group12App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
